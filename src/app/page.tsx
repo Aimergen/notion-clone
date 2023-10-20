c535f7627576fec1fc74ec11e100c826519921f9
Aimergen Adku <116338149+Aimergen@users.noreply.github.com>
2023-10-20 19:03:35 +0800

    added sign-in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Button>hello</Button>
+    <div className=" bg-gradient-to-r min-h-screen from-rose-100 to-teal-100">
+      <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7xl text-center">
+          Notion <span className=" text-purple-500">from ADKU</span>
+        </h1>
       </div>
-    </main>
+    </div>
   );
 }
